92c9998332decd35bca9ec186874617a9baeab47
jesciak <jesciak@yahoo.com>
2022-10-19 23:17:16 -0400

    added quiz questions

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var quizQuestions = [
+    {
+        questions: "Arrays in Javascript can be used to store _______________?",
+        choices: ["1. <numbers and strings>", "2. <other arrays>", "3. <booleans>", "4. <all of the above>"],
+        answer: 3,
+    },
+    {
+        questions:  "Commonly used data types DO NOT include:",
+        choices: ["1. <strings>", "2. <alerts>", "3. <booleans>", "4. <numbers>"],
+        answer: 1,
+    },
+    {
+        questions: "String values must be enclosed within ______________ when being assigned to variables.",
+        choices:["1. <quotes>", "2. <commas>", "3. <curly brackets>", "4. <parenthesis>"],
+        answer: 0,
+    },
+    {
+        questions: "A very useful tool used during developement and debugging for printing content to the debugger is:",
+        choices: ["1. <for loops>", "2. <JavaScript>", "3. <console.log>", "4. <terminal/bash>"],
+        answer: 2,
+    }
+    {
+        questions: "The condition in an if/else statement is enclosed with _____________.",
+        choices: ["1. <parenthesis>", "2. <quotes>", "3. <square brackets>", "4. <curly brackets>"],
+        answer: 0,
+    }
+]
