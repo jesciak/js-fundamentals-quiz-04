9c42618ceb0c3ac6fffa64e2bdfb8bf62918ab34
jesciak <jesciak@yahoo.com>
2022-11-03 02:52:21 -0400

    added lots more to script.js, and started some CSS styling

--- assets/script.js
+++ assets/script.js
@@ -6,10 +6,21 @@ var p =document.querySelector('p');
 var start =document.querySelector('#start');
 var choices =document.querySelector('#choices');
 var indicator =document.querySelector('#indicator');
+var textinput =document.querySelector('input');
+var form= document.querySelector('div');
+var next= document.querySelector('button');
+var back= document.querySelector('button');
+var formBtn= document.querySelector('button');
+var clear= document.querySelector('button');
+var viewhighScore=document.querySelector('#highscores');
 
-
-
-
+var countDown;
+var counterInterval;
+var highScore={
+    initials: '',
+    score: 0,
+};
+var scores=[];
 
 var quizQuestions = [
     {
@@ -38,27 +49,135 @@ var quizQuestions = [
         answer: 0,
     },
 ];
- 
-function init(){
-    countDown= 50;
-    countdownTrigger();
-}
 
-function countdownTrigger(){
-    if(countDown >0){
-        countDown--;
-        document.getElementById('start').innerHTML= countDown;
-        if (countDown>0){
-            counter = setTimeout('countdownTrigger()', 1000);
-        }
+    function init(){
+        countDown=50;
+        score=0;
     }
-}
-init();
-
+     function countdownTrigger(){
+       p.remove();
+        start.remove();
+        counterInterval =setInterval(function(){
+            countDown--;
+        countdown.textContent=countDown;
+            if(countDown===0){
+                clearTimer(counterInterval);
+            }
+        }, 1000);
+        renderQuestions();
+            }
+ 
 function renderQuestions (questionArr){
     questionArr= questionArr || 0;
+   var newQuestion= quizQuestions[questionArr];
+   h1.textContent=newQuestion.quizQuestions;
+
+   var multipleChoice= document.createElement('div');
+   choices.appendChild(multipleChoice);
    
+   for (var i=0; i<newQuestion.choices.length; i++){
+    var choice =newQuestion.choices[i];
+
+    var list=document.createElement('li');
+    list.setAttribute('list', i);
+    list.textContent=choice;
+    multipleChoice.appendChild(list);
+
+    list.addEventListener('click', function(event){
+        if(
+            newQuestion.answer===
+            parseInt(event.target.getAttribute('list')))
+        {
+            score +=10;
+            indicator.innerHTML='<h2>Correct!';
+                indicator.setAttribute('style', 'color: green');
+        }  else {
+                    countDown -=5;
+                    indicator.innerHTML='<h2>Wrong!';
+                        indicator.setAttribute('style', 'color: red');
+        };
+    
+        newQuestion++;
+
+        if(newQuestion===quizQuestions.length){
+            clearTimeout(counterInterval);
+            indicator.textContent='';
+            multipleChoice.remove();
+            h1.textContent="Quiz is over!";
+            h1.appendChild(p);
+            p.textContent="Your final score is: " + score;
+
+            renderForm();
+        }
+        else{
+            setTimeout(function(){
+                init(newQuestion);
+                multipleChoice.remove();
+                indicator.textContent='';
+            }, 1000);
+        }
+            });
+        }
+        }
+    function renderForm(){
+        form.textContent= 'Enter initials: ';
+       formBtn.textContent='Sumbit';
+       main.appendChild(form);
+       form.appendChild(textinput)
+       form.appendChild(formBtn);
+    }
+var highScore={
+    initials: '',
+    score: 0,
+};
+var scores=[];
+
+    function submitScore(){
+        var inputInitials= document.querySelector('input').value;
+
+        highScore.initials=inputInitials;
+        highScore.score= score;
+        localStorage.setItem('highschore', JSON.stringify(highScore));
+        main.innerHTML='';
+        viewhighScore.textContent='';
+        timer.textContent='';
+
+        console.log(highScore);
+        renderHighScores();
+
+        function renderHighScores(){
+            var storedScore=JSON.parse(localStorage.getItem('highScore'));
+            h1.innerHTML = 'Highscores';
+            main.appendChild(h1);
+            highScore.setAttribute('class', 'highScore');
+            highScore.textContent=`${storedScore.initials} - ${storedScore.score}`;
+            h1.appendChild(highScore);
+            back.textContent= 'Back';
+            next.textContent='Next';
+            clear.textContent='Clear';
+            main.appendChild(back);
+            main.appendChild(next);
+            main.appendChild(clear);  
+        }
+function clear(){
+    highScore.remove();
 }
+function back(){
+    location.reload();
+}
+viewhighScore.addEventListener('click', function(){
+p.remove();
+start.remove();
+renderHighScores();
+});
+    }
+
+start.addEventListener('click', countdownTrigger);
+next.addEventListener('click', next);
+back.addEventListener('click', back);
+formBtn.addEventListener('click', submitScore);
+clear.addEventListener('click', clear);
+
 
 // function init(){
 //     countDown=30;
@@ -76,3 +195,20 @@ function renderQuestions (questionArr){
 //     }, 1000);
 //         }
  
+// function init(){
+//     countDown= 50;
+//     countdownTrigger();
+// }
+
+// function countdownTrigger(){
+    
+//     if(countDown >0){
+//         countDown--;
+//         document.getElementById('timer').innerHTML= countDown;
+//         if (countDown>0){
+//             counter = setTimeout('countdownTrigger()', 1000);
+//         }
+      
+//     }
+// }
+// init();
